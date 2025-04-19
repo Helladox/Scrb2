b20fc6987d142f468340dbc947da4ed7960d0fd1
Helladox <koreanserverff444@gmail.com>
2025-04-19 23:09:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ YTUB_COOKIES = """
 
 API_ID = int(getenv("API_ID", "22581733"))
 API_HASH = getenv("API_HASH", "1db7bdcf908100cc641c6a5276765c3d")
-BOT_TOKEN = getenv("BOT_TOKEN", "7839616256:AAF9uxk5RpB4UPwvcB0NyG-cGru9URHTPQw")
+BOT_TOKEN = getenv("BOT_TOKEN", "7978077458:AAGlcsLBqSo9eDkrQ9rYLgyWBC0a3Rqqfxc")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "6530997270").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://rathourayush444:UOtUanJ8fbXqLMjB@cluster0.3anxc25.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = getenv("LOG_GROUP", "-1002294532244")
